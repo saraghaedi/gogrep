4b3ec3b08772aa1e590cc6673bcd2b9883e302b8
Sara Ghaedi <saraghaedi75@gmail.com>
2020-11-06 20:10:07 +0330

    feat: basic functionality of grep

--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func grepStdIn(input string) {
+	stdInReader := bufio.NewReader(os.Stdin)
+
+	grep(stdInReader, input)
+}
+
+func grepFile(fileAddress, input string) {
+	fileReader, err := os.Open(fileAddress)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	defer fileReader.Close()
+
+	grep(fileReader, input)
+}
+
+func grep(reader io.Reader, input string) {
+	scanner := bufio.NewScanner(reader)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, input) {
+			fmt.Println(line)
+		}
+	}
+}
 
 func main() {
-	fmt.Println("Grep implementation in Golang programming language!")
+	fPtr := flag.String("f", "Stdin", "Take patterns from file")
+
+	flag.Parse()
+
+	input := flag.Arg(0)
+
+	if fPtr != nil && *fPtr != "Stdin" {
+		grepFile(*fPtr, input)
+	} else {
+		grepStdIn(input)
+	}
 }
